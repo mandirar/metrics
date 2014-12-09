f8f8d24a06a8689d35468b77dae03ae523437aae
morganrae <morgane@mit.edu>
2014-12-08 20:21:37 -0500

    Version 10.2

---
+++ main.m
@@ -0,0 +1,41 @@
+% This script executes the optimization problem.
+
+clear all
+tic
+
+addpath('./tools')
+
+global_vars; %load global variables
+
+%% Calculate optimization inputs:
+guess = 10*rand(n,fuel_count);         %initial guess for fuel use
+A     = [];                            %linear inequality constraint...
+b     = [];                            %...of the form Ax <= b
+Aeq   = [];                            %linear equality constraint...
+beq   = [];                            %...of the form Ax = b
+lb    = zeros(n,fuel_count);           %lower bounds
+ub    = inf*ones(n,fuel_count);        %upper bounds
+% Note: there is also a non-linear equality and inequality constraint 
+% function, defined separately (constraint.m).
+
+%% Set optimization options:
+options             = optimset('fmincon'); 
+options.Algorithm   = 'interior-point';    
+options.MaxFunEvals = 10^7;
+options.MaxIter     = 10^7;
+options.TolX        = 10^-20;
+% Note: alogirthm options are 'trust-region-reflective', 'interior-point', 
+% 'sqp', and 'active-set.'
+
+%% Run optimization problem:
+[fuel_use,~,~,~,lambda] = fmincon(@(fuel_use) -objfun(fuel_use),guess,A,b,Aeq,beq,lb,ub,'constraint',options);
+
+plotResults(fuel_use,lambda.eqnonlin)
+
+toc
+
+lambda.eqnonlin(2)
+%lambda.eqnonlin(2) / (eta1/(Ak*ems1_k*(1/2.12)*24))
+lambda.eqnonlin(2) / (eta2/(1.4*Am*ems2_m*(353.8*1/1000)*24))
+%clear('A','Aeq','b','beq','first_year','form','fuel_count','guess',...
+%    'ide_s','last_year','lb','n','options','peak','t','ub','dt');
