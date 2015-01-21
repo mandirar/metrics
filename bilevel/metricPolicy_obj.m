a9f836c0a0ca0fbf505b2aab73ecc7dde316ef25
morganrae <morgane@mit.edu>
2015-01-21 11:08:04 -0500

    Simplified objective function for bilevel metric

--- bilevel/metricPolicy_obj.m
+++ bilevel/metricPolicy_obj.m
@@ -1,14 +1,10 @@
-function [ energy_all ] = metricPolicy_obj( metric,ems_mat,ek_constraint )
+function [ energy_all ] = metricPolicy_obj( metric )
 % This is the objective function for the policy's optimization problem.
 % The objective is to maximize the sum of energy use across all years.
 
 constants;                                 %loads constants
 
-fuel_use   = fuelUser_fun(metric,ems_mat,ek_constraint); %fuel user optimizaton problem  
-
-energy     = fuel_use * eta_vec;           %vector of energy use in each year
-
-energy_all = sum(energy)                  %sum of energy use across all years
+energy_all = sum(metric);
 
 end
 
